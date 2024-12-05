7548c4a99e5005a3d9a9b2eb468fdb38718f91e6
Chloe Avenas <avenaschloe@gmail.com>
2024-12-05 15:48:37 +0100

    building the app

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import pandas as pd
-import requests
+# import requests
 import numpy as np
+import time
+
+st.set_page_config(
+    page_title="Arrhythmia",
+    page_icon="❤️",
+    layout="centered",
+    initial_sidebar_state="auto")
 
 '''
-# 🫀🥼
+# ❤️🫀🥼
 '''
 
 st.markdown('''
@@ -20,19 +27,20 @@ uploaded_file = st.file_uploader(
 
 ## Preview of uploaded CSV
 if uploaded_file is not None:
-    dataframe = pd.read_csv(uploaded_file)
-    df = dataframe.drop(columns="target")
+    df = pd.read_csv(uploaded_file)
     st.markdown("Here's a preview of the data you just uploaded:")
-    st.write(df.head(1).values)
+    # st.write(df.T)
     # st.write(type(df.columns)) # => pandas list d'index
     # st.write(type(int(df.columns[10]))) # => pandas list d'index
     # st.write(type(df.values)) # => liste de floats
     # st.write(type(df.head(1).T[0]))
     # st.line_chart(x=df.head(1).T[0].index, y=df.head(1).T[0])
-    data = df.head(1).T
-    st.line_chart(data[0])
+    st.line_chart(df)
 
-## Button to classify heartbeats
-# st.button("Start", type="primary")
-if st.button("Start"):
-    st.write("Loading...")
+    ## Button to classify heartbeats
+    # st.button("Start", type="primary")
+    if st.button("Start prediction"):
+        # st.spinner('Loading...')
+        with st.spinner('Loading...'):
+            time.sleep(2)
+        st.success("This is an 'S' heartbeat.")
